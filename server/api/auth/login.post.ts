b4211918ed3dbd18b7db57f10f1f954362f7eb71
Ali Afghah <ali.afghah@gmail.com>
2025-03-04 14:42:29 +0330

    feat: added skeleton for login

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,1 +1,33 @@
 // /api/auth/login.post.ts
+import { z, ZodError } from 'zod'
+
+export default defineEventHandler(async (event) => {
+  // Read body
+  const { email, password } = await readBody(event)
+
+  // Create Zod schema
+  const userSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+  })
+
+  try {
+    // Validate user data
+    userSchema.parse({ email, password })
+
+    // Check if user exists
+
+    // Check if password is correct
+
+    // Set user session
+
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return createError({
+        statusCode: 400,
+        statusMessage: error.message,
+      })
+    }
+    throw error
+  }
+})
