88fef2e7d29001d18830a05c0d9196a29f3ab4d7
Ali Afghah <ali.afghah@gmail.com>
2025-03-06 14:10:29 +0330

    feat: implement login authentication with user validation and session management

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,5 +1,6 @@
 // /api/auth/login.post.ts
 import { z, ZodError } from 'zod'
+import { eq } from 'drizzle-orm'
 
 export default defineEventHandler(async (event) => {
   // Read body
@@ -16,16 +17,38 @@ export default defineEventHandler(async (event) => {
     userSchema.parse({ email, password })
 
     // Check if user exists
-
+    const getUserFromDb = await useDrizzle().select().from(tables.users).where(eq(tables.users.email, email))
+    const user = getUserFromDb[0]
+    if (!user) {
+      return createError({
+        statusCode: 400,
+        statusMessage: 'Invalid credentials',
+      })
+    }
     // Check if password is correct
+    const isPasswordCorrect = await verifyPassword(user.password, password)
+    if (!isPasswordCorrect) {
+      return createError({
+        statusCode: 400,
+        statusMessage: 'Invalid credentials',
+      })
+    }
 
     // Set user session
+    await setUserSession(event, {
+      user: {
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+      },
+      loggedInAt: new Date()
+    })
 
   } catch (error) {
     if (error instanceof ZodError) {
       return createError({
         statusCode: 400,
-        statusMessage: error.message,
+        statusMessage: error.errors[0].message,
       })
     }
     throw error
