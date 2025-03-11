8e43f31c2042a58edbcde0542fa6e0abc5ddc258
Ali Afghah <ali.afghah@gmail.com>
2025-03-11 17:29:15 +0330

    refactor: improve home page user session display and styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,21 @@
 <script lang="ts" setup>
-const { loggedIn, user, session, clear } = useUserSession()
-const router = useRouter()
-
-onBeforeMount(() => {
-	if (!loggedIn) {
-		router.push('/login')
-	}
-})
+const { loggedIn, session } = useUserSession()
 </script>
 
 <template>
 	<div v-if="loggedIn">
-		<h1>Hello {{ user?.firstName }} {{ user?.lastName }}</h1>
-		<pre>
-			{{ session }}
+		<h1>
+			Hello
+			<span class="font-bold">{{ session?.user?.firstName + ' ' + session?.user?.lastName }}</span>
+		</h1>
+		<br />
+		<pre class="text-sm font-mono whitespace-pre-wrap bg-gray-100 p-4 rounded-md">
+			{{ JSON.stringify(session, null, 2) }}
 		</pre
 		>
 	</div>
 	<div v-else>
-		<h1>Please login to continue</h1>
-		<NuxtLink to="/login">Login</NuxtLink>
+		<h1 class="text-2xl font-bold">Please login to continue</h1>
+		<NuxtLink to="/login" class="text-blue-500">Login</NuxtLink>
 	</div>
 </template>
