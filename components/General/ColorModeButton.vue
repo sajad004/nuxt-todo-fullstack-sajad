85ae045607b47d6e90869c118dd02d71fe3a3f4c
Ali Afghah <ali.afghah@gmail.com>
2025-04-10 14:08:23 +0330

    fix: update icon references in ColorModeButton component to use heroicons

--- components/General/ColorModeButton.vue
+++ components/General/ColorModeButton.vue
@@ -14,7 +14,7 @@ const isDark = computed({
 <template>
 	<ClientOnly v-if="!colorMode?.forced">
 		<UButton
-			:icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'"
+			:icon="isDark ? 'i-heroicons-moon' : 'i-heroicons-sun'"
 			color="neutral"
 			variant="ghost"
 			@click="isDark = !isDark"
